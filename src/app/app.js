ae39983ccb7bcc0613406662993fff2db561ecc7
Anurag Simgeker <anuragsimgeker@gmail.com>
2015-07-21 09:46:53 -0700

    Removing coffee related, using Phantomjs instead of Firefox

--- src/app/app.js
+++ src/app/app.js
@@ -32,7 +32,7 @@ angular.module( 'ngBoilerplate', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $mdSidenav, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
+      $scope.pageTitle = toState.data.pageTitle + ' | Pros & Cons';
     }
   });
 
